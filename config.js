64275b465e038cd551d2d37a4f06a5440b1fbcaf
lobbyt <154092980+lobbyt@users.noreply.github.com>
2025-07-24 01:50:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ const toBool = (value) => value === "true";
 
 // Owner & Bot Details
 global.owner = process.env.OWNER_NUMBER;
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.anitav4 = process.env.BOT_NAME || "King lobby-𝐕𝟒";
+global.ownername = process.env.OWNER_NAME || "Lobby";
+global.packname = process.env.PACK_NAME || "𝐕𝟒";
+global.author = process.env.AUTHOR || "Gracious";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 
